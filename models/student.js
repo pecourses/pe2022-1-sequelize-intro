f8fb5086c1be8db099976fcdbb28ece486987170
Viktoriia <pecourses>
2023-04-01 17:54:47 +0300

    Update model definition

--- models/student.js
+++ models/student.js
@@ -12,14 +12,60 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
+  /* Обмеження
+PRIMARY KEY
+UNIQUE - unique (constraint)
+CHECK - validate (validator)
+NOT NULL - allowNull (validator + constraint),
+FOREIGN KEY
+
+Дефолтне значення
+DEFAULT - defaultValue
+*/
+  // Student.create({})
+  // validator -> db (contaraints)
   Student.init(
     {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      birthday: DataTypes.DATE,
-      isMale: DataTypes.BOOLEAN,
-      activitiesCount: DataTypes.INTEGER,
+      firstName: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+        validate: {
+          is: /^[A-Z][a-z]+(-[A-Z][a-z]+)?$/,
+          len: [2, 64],
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING(64),
+        validate: {
+          is: /^[A-Z][a-z]+(-[A-Z][a-z]+)?$/,
+          len: [2, 64],
+        },
+      },
+      email: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      birthday: {
+        type: DataTypes.DATEONLY,
+        validate: {
+          isDate: true,
+          isBefore: new Date().toISOString(),
+        },
+      },
+      isMale: {
+        type: DataTypes.BOOLEAN,
+      },
+      activitiesCount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+        },
+      },
     },
     {
       sequelize,
